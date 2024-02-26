f6249478c6a1218fa704e7937b4fba24697fc844
Darkhan <135841516+saint1st@users.noreply.github.com>
2024-02-26 10:46:46 +0000

    adding import fix

--- test_corenlp.py
+++ test_corenlp.py
@@ -1,4 +1,4 @@
-from nlplogic.corenlp import search_wikipedia, summarise_wikipedia, get_text_blob, get_phrases
+from nlplogic.corenlp import get_phrases
 
 def test_get_phrase():
     assert 'golden state' in get_phrases("Golden State Warriors")
